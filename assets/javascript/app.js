9f2826494165f3850685c3a50312c1f7f5892c69
Cassandra Claire <cassandra_villaruel@yahoo.com>
2017-11-25 17:35:56 -0600

    Creating my Portfolio

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+$('#example2').typeIt({
+    strings: ["develop websites.", "create interactive experiences.", "implement and manage databases.", "am a Web Developer."],
+    speed: 100,
+    breakLines: false,
+    deleteDelay: 2000,
+    autoStart: false,
+    loop: false
+})
+
